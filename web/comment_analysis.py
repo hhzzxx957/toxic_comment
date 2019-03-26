febf44b7eec1faf6ff1f01df9e19b11a973bd1a8
hhzzxx957 <hzx957@vt.edu>
2019-03-26 15:09:39 -0400

    first_commit

---
+++ web/comment_analysis.py
@@ -0,0 +1,42 @@
+
+import numpy as np, pandas as pd
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+from keras.models import Model
+from keras.models import model_from_json
+from keras import backend
+
+import pickle
+
+def com_analysis(comment):
+    max_features = 20000
+    maxlen = 100
+
+    test=pd.DataFrame([comment])
+    test.columns=['comment_text']
+    list_sentences_test = test["comment_text"].fillna("_na_").values
+
+    # loading
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+    list_tokenized_test = tokenizer.texts_to_sequences(list_sentences_test)
+    X_te = pad_sequences(list_tokenized_test, maxlen=maxlen)
+
+    json_file = open('model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("model.h5")
+    print("Loaded model from disk")
+    loaded_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    y_test = loaded_model.predict([X_te], batch_size=1024, verbose=1)
+    backend.clear_session()
+    return y_test
+
+# comment='This is a good game!!'
+# output=com_analysis(comment)
+# print(output)
---
+++ web/miniword.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, validators
+import comment_analysis
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+# App config.
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+# app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://hzx957:sql104957@hzx957.mysql.pythonanywhere-services.com/hzx957$toxic'
+db=SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__='toxic'
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.id}', '{self.date_posted}')"
+
+import io
+from flask import Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import matplotlib.pyplot as plt
+import numpy as np
+
+class ReusableForm(Form):
+    comment = TextField('Comment:', validators=[validators.required()])
+
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+    global percentage
+    percentage=[[0,0,0,0,0,0]]
+    form = ReusableForm(request.form)
+
+    print(form.errors)
+    if request.method == 'POST':
+        comment=request.form.get("comment", False)
+        print(comment)
+
+        if form.validate():
+            user=User(content=comment)
+            db.session.add(user)
+            db.session.commit()
+            flash('Thanks for input ')
+        else:
+            flash('Error: Can not be empty. ')
+
+        percentage=comment_analysis.com_analysis(comment)
+
+    return render_template('hello.html', form=form)
+
+@app.route('/plot.png', methods=['GET', 'POST'])
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def create_figure():
+    fig, ax = plt.subplots()
+    x = np.arange(6)
+    # ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
+    ax.set_frame_on(False)
+    plt.bar(x, percentage[0])
+    plt.ylim((0,1))
+    plt.xticks(x, ('toxic','severe_toxic','obscence','threat','insult','identity_hate'))
+    for i, v in enumerate(percentage[0]):
+        plt.text(x[i] - 0.25, v + 0.01, str(round(v, 2)))
+    return fig
+
+if __name__ == "__main__":
+    app.run()
